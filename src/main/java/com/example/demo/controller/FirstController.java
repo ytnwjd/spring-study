bc4a24c086fcd682b70b80b3ef237fe6164a5b3c
y.tnwjd <ysjsw@ajou.ac.kr>
2024-10-05 01:32:30 +0900

    git init & first commit

---
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username", "유수정");
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname", "유수정");
+        return "goodbye";
+    }
+}
