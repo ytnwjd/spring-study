00ab54d0f3c5409e71c41114ed128c2990748860
y.tnwjd <ysjsw@ajou.ac.kr>
2024-10-05 01:47:08 +0900

    ~chapter 4 complete

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.dto.ArticleForm;
 import com.example.demo.entity.Article;
 import com.example.demo.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired
@@ -21,11 +23,15 @@ public class ArticleController {
     @PostMapping("articles/create")
     public String createArticle(ArticleForm form){
 
+        log.info(form.toString());
+
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        // System.out.println(article.toString());
+        log.info(article.toString());
 
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        // System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return "";
     }
--- src/main/java/com/example/demo/entity/Article.java
+++ src/main/java/com/example/demo/entity/Article.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 @Entity
 public class Article {
 
@@ -18,18 +22,7 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
+    public Article() {
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
     }
 }
